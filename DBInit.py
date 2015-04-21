30110fff7b87ef19f46792cbc54226b9a74594a1
sealiu <iliuyang@foxmail.com>
2015-04-21 19:52:48 +0800

    增加约束

--- DBInit.py
+++ DBInit.py
@@ -5,27 +5,27 @@ conn = sqlite3.connect('db_pymemo.db')
 c = conn.cursor()
 c.execute(
     '''CREATE TABLE library (
-libId text,
-name text,
-libDesc text,
+libId text PRIMARY KEY NOT NULL UNIQUE,
+name text NOT NULL,
+libDesc text NOT NULL,
 createTime text,
-maxReviewsPerDay integer,
-newCardsPerDay integer,
-easyInterval integer,
-maxInterval integer,
-maxAnswerTime integer,
-isShowAnswerTime boolean
+maxReviewsPerDay integer DEFAULT 50,
+newCardsPerDay integer DEFAULT 50,
+easyInterval integer DEFAULT 3,
+maxInterval integer DEFAULT 3650,
+maxAnswerTime integer DEFAULT 30,
+isShowAnswerTime boolean DEFAULT True
 )''')
 
 c.execute(
     '''CREATE TABLE record (
-recordId text,
+recordId text PRIMARY KEY NOT NULL UNIQUE,
 ques text,
 ans text,
 addTime text,
 reviewTime text,
 alertTime text,
-interval integer,
-EF integer,
-isPaused boolean
+interval integer DEFAULT -1,
+EF integer DEFAULT 2.5,
+isPaused boolean DEFAULT FALSE
 )''')
