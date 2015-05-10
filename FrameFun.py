55401e2d884bd61686043ee06e4d4937fd1c8a46
sealiu <iliuyang@foxmail.com>
2015-05-10 13:41:28 +0800

    过滤器函数，返回过滤后的records_list

--- FrameFun.py
+++ FrameFun.py
@@ -1,49 +1,113 @@
 # -*- coding: utf-8 -*-
-
-# f = open('recordstack.pm', 'r')
-# allLines = [line.strip() for line in f.readlines()]
-# f.close()
-# for eachLine in allLines:
-#     print eachLine
 import DBFun
 import time
+import datetime
 
 
-def find_new(i):
+def find_all():
+    conn = DBFun.connect_db('db_pymemo.db')
+    conn.text_factory = str
+    sql = "SELECT * FROM record"
+    cursor = DBFun.select(conn, sql)
+    all_record = cursor.fetchall()
+    return all_record
+
+
+def find_new():
     """
-    随机返回指定词库中的新记录
-    :param i: lib_id
+    找到新记录并返回
     :return:
     """
     conn = DBFun.connect_db('db_pymemo.db')
-    sql = "SELECT * FROM record WHERE interval = -1 AND recordId LIKE '%" + i + "'"
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE interval = -1"
     cursor = DBFun.select(conn, sql)
-    return cursor.fetchall()
+    new_record = cursor.fetchall()
+    DBFun.close_db(conn)
+    return new_record
 
 
-def find_expired(i):
+def find_expired():
     """
-    找到指定词库中已经到期的记录并返回
-    :param i:
+    找到已经到期的记录并返回
     :return:
     """
+    expired_list = []
     conn = DBFun.connect_db('db_pymemo.db')
-    sql = "SELECT * FROM record WHERE interval = -1 AND reviewTime IS NOT NULL"
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE interval != -1"
     cursor = DBFun.select(conn, sql)
+    today = datetime.date.today()
     for rows in cursor.fetchall():
-        print time.mktime(time.strptime(rows[4], '%Y/%m/%d'))
-    # return cursor.fetchall()
+        interval = rows[6]
+        review_time = rows[4]
+        # 下次应该复习时间
+        review_time_date = datetime.datetime.strptime(review_time, '%Y/%m/%d').date()
+        a_time_date = review_time_date + datetime.timedelta(days=interval)
+        # 比较秒数
+        a_time = time.mktime(time.strptime(str(a_time_date), '%Y-%m-%d'))
+        b_time = time.mktime(time.strptime(str(today), '%Y-%m-%d'))
+        if b_time >= a_time:
+            expired_list.append(rows)
+    DBFun.close_db(conn)
+    return expired_list
 
 
-def find_remembered(i):
+def find_remembered():
     """
-    找到指定词库中已经记住的记录并返回
-    :param i: 词库id
+    找到已经记住的记录并返回
     :return:
     """
     conn = DBFun.connect_db('db_pymemo.db')
-    sql = "SELECT * FROM record WHERE EF>=3.0 AND recordId LIKE '%" + i + "'"
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE EF>=3.0"
     cursor = DBFun.select(conn, sql)
-    return cursor.fetchall()
+    remembered_list = cursor.fetchall()
+    DBFun.close_db(conn)
+    return remembered_list
 
-find_expired('000')
+
+def find_learned():
+    today = datetime.date.today().strftime('%Y/%m/%d')
+    conn = DBFun.connect_db('db_pymemo.db')
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE reviewTime == '" + today + "'"
+    cursor = DBFun.select(conn, sql)
+    learned_record = cursor.fetchall()
+    DBFun.close_db(conn)
+    return learned_record
+
+
+def find_hard():
+    """
+    找到始终记不住的记录并返回
+    :return:
+    """
+    conn = DBFun.connect_db('db_pymemo.db')
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE EF=1.3"
+    cursor = DBFun.select(conn, sql)
+    hard_list = cursor.fetchall()
+    DBFun.close_db(conn)
+    return hard_list
+
+
+def find_today():
+    """
+    找到今天添加的记录并返回
+    :return:
+    """
+    today_record = []
+    conn = DBFun.connect_db('db_pymemo.db')
+    conn.text_factory = str
+    sql = "SELECT * FROM record WHERE interval = -1"
+    cursor = DBFun.select(conn, sql)
+
+    for rows in cursor.fetchall():
+        add_date = rows[3]
+        add_time = time.mktime(time.strptime(str(add_date), '%Y/%m/%d'))
+        now_time = time.mktime(time.strptime(str(datetime.date.today()), '%Y-%m-%d'))
+        if add_time == now_time:
+            today_record.append(rows)
+    DBFun.close_db(conn)
+    return today_record
