35504d007efb2ea92a3425da776e6d717ec5abab
sealiu <iliuyang@foxmail.com>
2015-05-13 13:20:02 +0800

    对recordstack.pm进行读写操作，读取统计数字

--- recordstack_test.py
+++ recordstack_test.py
@@ -1,1 +1,18 @@
-__author__ = 'sealiu'
+# -*- coding: utf-8 -*-
+# 对recordstack.py进行操作
+
+
+def fetch_statistic():
+    f = open("recordstack.pm", 'r')
+    statistic = {}
+    for eachLine in f:
+        if eachLine.strip()[0] != '#':
+            statistic[eachLine[0]] = int(eachLine[2])
+    f.close()
+    return statistic
+
+
+def write_statistic():
+    f = open("recordstack.pm", 'w') 
+
+print fetch_statistic()
