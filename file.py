b9a538d2c113b98d7d7ccf86531ac1a462c00aa1
sealiu <iliuyang@foxmail.com>
2015-05-14 21:08:29 +0800

    set_statistic,设置NSR的统计数据。在写入学习内容的同时更新NSR统计数据

--- file.py
+++ file.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+import re
 import FrameFun
 import os
 
 
 def fetch_statistic(fn):
+    """
+    获取NSR的统计数据
+    :param fn:
+    :return:
+    """
     f = open(fn, 'r')
     statistic = {}
     for eachLine in f:
@@ -12,17 +19,51 @@ def fetch_statistic(fn):
     return statistic
 
 
+def set_statistic(fn, name, value):
+    """
+    设置NSR的统计数据
+    :param fn:
+    :param name:
+    :param value:
+    :return:
+    """
+    f1 = open(fn)
+    f1_list = f1.readlines()
+    f1.close()
+    if name == 'N':
+        f1_list[0] = 'N:' + str(value) + '\n'
+    elif name == 'S':
+        f1_list[1] = 'S:' + str(value) + '\n'
+    elif name == 'R':
+        f1_list[2] = 'R:' + str(value) + '\n'
+    f2 = open(fn, 'w+')
+    f2.writelines(f1_list)
+
+
 def write_rs(fn, ls, v):
+    """
+    向指定文件中写入待学习内容，并更新NSR统计数据
+    :param fn:file_name
+    :param ls:NSR_list
+    :param v:NSR
+    :return:
+    """
     f = open(fn, 'a')
     for rows in ls:
         f.write('\n')
         for item in FrameFun.tuple_add_front(rows, v):
             f.write('%s%s' % (item, ' '))
     f.close()
+    set_statistic(fn, v, len(ls))
     return True
 
 
 def is_exist(url, name):
+    """
+    文件name是否存在路径url下
+    :param url:
+    :param name:
+    :return:
+    """
     dir_ls = os.listdir(url)
     return dir_ls.count(name)
-
