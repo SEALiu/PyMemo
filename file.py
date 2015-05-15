3537838c20aaf962fc95f67ec5dc52b54a8bfe51
sealiu <iliuyang@foxmail.com>
2015-05-15 20:29:38 +0800

    修改fetch_statistic，不再是读取前三行而是统计每一类的个数； 增加4个函数，read_rs, is_exist, exist_id, filter_repeat

--- file.py
+++ file.py
@@ -11,11 +11,20 @@ def fetch_statistic(fn):
     :return:
     """
     f = open(fn, 'r')
+    n = 0
+    s = 0
+    r = 0
     statistic = {}
     for eachLine in f:
-        if eachLine.strip()[0] != '#':
-            statistic[eachLine[0]] = int(eachLine[2])
-    f.close()
+        if eachLine.strip()[0] == 'N':
+            n += 1
+        elif eachLine.strip()[0] == 'S':
+            s += 1
+        elif eachLine.strip()[0] == 'R':
+            r += 1
+    statistic['N'] = n - 1
+    statistic['S'] = s - 1
+    statistic['R'] = r - 1
     return statistic
 
 
@@ -27,9 +36,7 @@ def set_statistic(fn, name, value):
     :param value:
     :return:
     """
-    f1 = open(fn)
-    f1_list = f1.readlines()
-    f1.close()
+    f1_list = read_rs(fn)
     if name == 'N':
         f1_list[0] = 'N:' + str(value) + '\n'
     elif name == 'S':
@@ -38,6 +45,7 @@ def set_statistic(fn, name, value):
         f1_list[2] = 'R:' + str(value) + '\n'
     f2 = open(fn, 'w+')
     f2.writelines(f1_list)
+    f2.close()
 
 
 def write_rs(fn, ls, v):
@@ -54,8 +62,15 @@ def write_rs(fn, ls, v):
         for item in FrameFun.tuple_add_front(rows, v):
             f.write('%s%s' % (item, ' '))
     f.close()
-    set_statistic(fn, v, len(ls))
+    set_statistic(fn, v, fetch_statistic(fn)[v])
     return True
+
+
+def read_rs(fn):
+    f = open(fn)
+    f_list = f.readlines()
+    f.close()
+    return f_list
 
 
 def is_exist(url, name):
@@ -67,3 +82,28 @@ def is_exist(url, name):
     """
     dir_ls = os.listdir(url)
     return dir_ls.count(name)
+
+
+def exist_id(fn):
+    """
+    返回fn文件中已经存在的记录的id列表
+    :param fn: file_name
+    :return:
+    """
+    exist_ls = []
+    result_ls = read_rs(fn)
+    for index, rows in enumerate(result_ls):
+        if index >= 3:
+            exist_ls.append(rows[2:10])
+    return exist_ls
+
+
+def filter_repeat(fn, ls):
+    e_id = exist_id(fn)
+    ls_temp = []
+    for index, item in enumerate(ls):
+        if e_id.count(item[0]) != 0:
+            ls_temp.append(item)
+    for index in ls_temp:
+        ls.remove(index)
+    return ls
